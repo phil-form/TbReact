8b25722fe16e82d98e5bed541cbb5643bafb0556
clbr-odoo <clbr@odoo.com>
2022-08-16 15:01:26 +0200

    ProductList exercice

---
+++ src/exercice/Claire/exercice_01/src/components/exo_02/ProductList.jsx
@@ -0,0 +1,42 @@
+
+function ProductList({products}) {
+    const productsJsx = products.map(
+        (product) => <ProductTableRow key={product.id} {...product}/>
+    );
+
+    return (
+        <>
+            <h2>List of products</h2>
+            <table>
+                <ProductTableHead/>
+                <tbody>
+                    {productsJsx}
+                </tbody>
+            </table>
+        </>
+    );
+}
+
+function ProductTableHead () {
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Promo</th>
+            </tr>
+        </thead>
+    );
+};
+
+function ProductTableRow ({ name, price, promo }) {
+    return (
+        <tr className={promo ? 'promo' : ''}>
+            <td>{name}</td>
+            <td>{price}</td>
+            <td>{promo ? "En promo" : ""}</td>
+        </tr>
+    );
+}
+
+export default ProductList;
