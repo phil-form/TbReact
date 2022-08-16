2497c32c6cae92f5c10cbb09213bf20e6b51e015
philippe.stons <philippe.stons@bstorm.be>
2022-08-16 13:57:37 +0200

    [EXERCICE 01] hello world avec props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Welcome from "./demo/demo-01/components/welcome";
+import HelloPerson from "./exercice/exercice-01/components/hello-person";
 
 function App() {
   return (
     <div className="App">
-      <Welcome firstname="Zaza" lastname="Duck" />
+        {/* demo 1*/}
+        {/*<Welcome firstname="Zaza" lastname="Duck" />*/}
+        {/* exercice 1*/}
+        <HelloPerson name='Riri' age={18}/>
     </div>
   );
 }
