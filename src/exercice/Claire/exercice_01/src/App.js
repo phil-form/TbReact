ea935b9ed26546014c06edd6a77c4d3968552728
clbr-odoo <clbr@odoo.com>
2022-08-16 16:58:07 +0200

    Exercice 03 compteur states

--- src/exercice/Claire/exercice_01/src/App.js
+++ src/exercice/Claire/exercice_01/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 // import WelcomeAge from './components/exo_01/WelcomeAge';
-import ProductList from './components/exo_02/ProductList';
-import products from './data/products.json'
+// import ProductList from './components/exo_02/ProductList';
+// import products from './data/products.json'
+import Counter from './components/exo_03/Counter';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       {/* EXO_01*/}
       {/* <WelcomeAge name="Riri"/> */}
-      <ProductList products={products}/>
+      {/* <ProductList products={products}/> */}
+      <Counter toIncrement={2}/>
     </div>
   );
 }
