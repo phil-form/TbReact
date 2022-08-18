cde2aefad85228d70491b0aa975033a23a39dd8e
philippe.stons <philippe.stons@bstorm.be>
2022-08-18 15:13:47 +0200

    [DEMO] onEffect

--- src/exercice/exercice-05/components/task-list/task-list.jsx
+++ src/exercice/exercice-05/components/task-list/task-list.jsx
@@ -1,15 +1,42 @@
 import TaskListItem from "./task-list-item";
+import {useState} from "react";
+import PriorityEnum from "../../enums/priority-enum";
+import style from './task-list.module.css'
 
 const TaskList = ({ tasks, onDeleteTask, onDoneTask }) =>
 {
-    const tasksJsx = tasks.map((task, index) =>
-        <TaskListItem key={index} id={index} {...task} onDelete={onDeleteTask} onDone={onDoneTask} />
+    const [filterValue, setFilterValue] = useState('ALL');
+    
+    const tasksJsx = tasks.filter((value) => {
+        switch (filterValue)
+        {
+            case 'PRIORITY':
+                return value.priority === PriorityEnum.HIGH;
+            case 'DONE':
+                return value.isDone;
+            case 'PENDING':
+                return !value.isDone;
+            default:
+                return true;
+        }
+    }).map((task, index) => {
+            return <TaskListItem key={index} id={index} {...task} onDelete={onDeleteTask} onDone={onDoneTask}/>
+        }
     );
 
+    console.log(tasksJsx);
     return (
-        <ul>
-            {tasksJsx}
-        </ul>
+        <>
+            <select name='priority' value={filterValue} onChange={(e) => setFilterValue(e.target.value)}>
+                <option value='ALL'>ALL</option>
+                <option value='PRIORITY'>PRIORITY</option>
+                <option value='DONE'>DONE</option>
+                <option value='PENDING'>PENDING</option>
+            </select>
+            <ul className={style.tasklist}>
+                {tasksJsx}
+            </ul>
+        </>
     );
 }
 
